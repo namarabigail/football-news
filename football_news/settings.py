ae32bc642921b4f3bc18ae607083a8707c20b7f7
namarabigail <bigelnp10@gmail.com>
2025-09-24 15:34:24 +0700

    cleanup

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "abigail-namaratonggi-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://abigail-namaratonggi-footballnews.pbp.cs.ui.ac.id/"
+    "https://abigail-namaratonggi-footballnews.pbp.cs.ui.ac.id"
 ]
 # Application definition
 
